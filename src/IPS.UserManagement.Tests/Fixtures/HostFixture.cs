e618d1c7fe0e93f9eb499a47d03db957532fcc03
Uroš Miletić <uros.miletic@ips-ag.com>
2022-08-08 14:22:41 +0200

    Add implementation base (#6)
    * Add webapi template project
    
    * Add Serilog and .editorconfig
    
    * Add continuous integration workflow
    
    * Remove restore step
    
    * Improve CI pipeline
    
    * Fix workspace name
    
    * Replace build configuration with env variable
    
    * Add tests
    
    * Add test report permissions
    
    * Rename report step
    
    * Add status badge
    
    * Use file-scoped namespaces
    
    * Add host start test

---
+++ src/IPS.UserManagement.Tests/Fixtures/HostFixture.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace IPS.UserManagement.Tests.Fixtures;
+
+public class HostFixture : IDisposable
+{
+    private readonly Lazy<WebApplicationFactory<Program>> _serverLazy;
+    public IServiceProvider Services => _serverLazy.Value.Services;
+    public HttpClient Client => _serverLazy.Value.CreateClient();
+    public ITestOutputHelper? TestOutputHelper { private get; set; }
+
+    public HostFixture()
+    {
+        _serverLazy = new Lazy<WebApplicationFactory<Program>>(() =>
+        {
+            return new WebApplicationFactory<Program>().WithWebHostBuilder(
+                webHost => webHost
+                    .ConfigureLogging((_, logging) =>
+                    {
+                        if (TestOutputHelper is null) return;
+                        logging.AddXunit(TestOutputHelper, LogLevel.Warning);
+                    })
+                    .UseEnvironment("Test")
+                    .UseContentRoot(Path.GetDirectoryName(GetType().Assembly.Location))
+                    .ConfigureAppConfiguration((context, config) =>
+                    {
+                        config.Sources.Clear();
+                        config.AddJsonFile(
+                            $"appsettings.{context.HostingEnvironment.EnvironmentName}.json",
+                            false,
+                            true)
+                        .AddEnvironmentVariables();
+                    })
+                    .ConfigureTestServices(AddTestServices)
+            );
+        });
+    }
+
+    public void Dispose()
+    {
+        if (!_serverLazy.IsValueCreated) return;
+        Client.Dispose();
+        _serverLazy.Value.Dispose();
+    }
+
+    private void AddTestServices(IServiceCollection services)
+    {
+    }
+}
---
+++ src/IPS.UserManagement/Program.cs
@@ -0,0 +1,37 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog(
+        (ctx, lc) => lc
+            .WriteTo.Console()
+            .ReadFrom.Configuration(ctx.Configuration));
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Error running application");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+public partial class Program { }
